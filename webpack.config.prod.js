0b43e5cce8fc6d6146e7c919696891d0c623a89c
donsijo <donsijo19@gmail.com>
2024-09-23 19:08:00 -0500

    chnagesmadedtowebpack

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -2,9 +2,15 @@ const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
+const path = require('path'); // Import path for resolving paths
 
 module.exports = merge(common, {
   mode: 'production',
+  output: {
+    filename: 'bundle.js', // Output file name for the bundled JavaScript
+    path: path.resolve(__dirname, 'public'), // Output directory for build files
+    clean: true, // Clean the output directory before emitting files
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: './index.html',
